ff34543a99e38f610a19f0ad9b58d581284239bd
Akerele, Tosin James <akerele.james1699@gmail.com>
2021-02-14 19:37:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const form = document.getElementById('form');
+// LINE 3-6 IS TO GET THE RESPECTIVE INPUT FIELDS.
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-
-function showError(input, displayMessage) {
+// FUNCTIONS DEFINITIONS
+function showError(input, displayMessage) { //input serves as the input-field-type.
     const addParentClass = input.parentElement;
     addParentClass.className = "portion error";
     const small = addParentClass.querySelector('small');
     small.innerText = displayMessage;
+    // return false;
 }
 
 function showSuccess(input) {
@@ -22,32 +24,54 @@ function validateEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+function checkRequired(inputArr){
+    inputArr.forEach(element => {
+        console.log(element.value);
+        if(element.value === "") {
+            showError(element, `${element.id.slice(0,1).toUpperCase() + element.id.slice(1)} is required`);
+        } else {
+            showSuccess(element);
+        }
+    });
+}
 
+ 
 form.addEventListener('submit', (e)=> {
     e.preventDefault(); //This prevents the form from refreshing which is the normal modus operanding after pressing a submit button. 
     // "Another way to achieve this is by using this syntax; onsubmit="return false" as the dom eventlistener in the HTML file
     
-    if(username.value==="") {
-        showError(username, "Username is required")
-    } else {
-        showSuccess(username);
-    }
+    // if(username.value==="") {
+    //     showError(username, "Username is required");
+    // } else {
+    //     showSuccess(username);
+    // }
 
-    if(email.value==="" || validateEmail(email.value)===false) {
-        showError(email, "Please enter a valid email")
-    } else {
-        showSuccess(email);
-    }
+    // if(email.value==="" || validateEmail(email.value)===false) {
+    //     showError(email, "Please enter a valid email address")
+    // } else {
+    //     showSuccess(email);
+    // }
 
-    if(password.value==="") {
-        showError(password, "Password is required")
-    } else {
-        showSuccess(password);
-    }
+    // if(password.value==="") {
+    //     showError(password, "Password is required")
+    // } else {
+    //     showSuccess(password);
+    // }
 
-    if(password2.value!==password.value || password2.value==="") {
-        showError(password2, "Password mismatch! Type again");
-    } else {
-        showSuccess(password2);
-    }
+    // if(password2.value==="") {
+    //     showError(password2, "Password is required");
+    //     console.log(password2.value)
+    // } else if(password2.value==="" && password2.value!==password.value) {
+    //     showError(password2, "Password mismatch! Type again");
+        
+    // }
+    // else {
+    //     showSuccess(password2);
+    // }
+
+    // LINE 32 to 59 is a wonderful approach to getting this done but it seem to be very messy. So, in order to clean this up, we shall introduce a more robust, cleaner and shorter lines of code to execute the same program.
+
+    // This new concept would have a function that would take in an array of the inputs (i.e. username, email, password & password2) and use them as arguements for the function.
+    const arrayArg = [username, email, password, password2];
+    checkRequired(arrayArg);
 })
